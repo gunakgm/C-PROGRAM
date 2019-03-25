16744b9d2ea80f7756301898b4c44ccd3e1b55cf
gunakgm <47106387+gunakgm@users.noreply.github.com>
2019-03-25 10:16:36 +0530

    Create 1.c

---
+++ player/set 2/1.c
@@ -0,0 +1,16 @@
+
+#include <stdio.h>
+int main()
+{
+  	char a[10];
+	 scanf("%s",a);
+ if(a[0]=='S'||a[0]=='s')
+	{
+	     printf("yes...holiday");
+	}
+	 else
+	{
+	  printf("no..not a holiday");   
+	}
+	  return 0;
+}
