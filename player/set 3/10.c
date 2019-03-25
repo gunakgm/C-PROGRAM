3690036706d704ecb8ee34bfdd36042bfcd8a05d
gunakgm <47106387+gunakgm@users.noreply.github.com>
2019-03-25 12:24:47 +0530

    Create 10.c

---
+++ player/set 3/10.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+void main()
+{
+   int st1,st2.c1,c2.k;
+  for x in st1;
+     if x not in xt1;
+    c1=c1+c2;
+    for x is st2;
+    if x not in st1
+    c2=c2+1;
+    if(c1==c2==k)
+    {
+    printf("yes");
+      }
+      else
+    {
+    printf("no");
+  }
+  }
